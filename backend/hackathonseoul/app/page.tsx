e93d28e966c499c6d5cd578e6ed6009088c566a1
Nanthawat Sukkam <nsu@MacBook-Pro-khxngNanthawat.local>
2025-08-04 00:50:55 +0700

    remove:remove frontend in backend

--- backend/hackathonseoul/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import UploadVideo from "@/components/videoUpload";
-export default async function Home() {
-  return (
-    <div>
-      <h1 className="text-4xl font-bold text-center">Welcome to Accident CCTV Detection System</h1>
-      <UploadVideo />
-    </div>
-  );
-}
